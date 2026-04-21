d5b2136d941ce5daf4f715e05f1b2da9232c1047
Raico <wiggins54626@gmail.com>
2026-04-20 20:51:06 -0500

    actualizando main

--- src/main/java/com/example/interfaces/Volador.java
+++ src/main/java/com/example/interfaces/Volador.java
@@ -1,5 +1,23 @@
 package com.example.interfaces;
 
-public class Volador {
+/**
+ * Interfaz Volador
+ * Define el comportamiento de las criaturas que pueden volar.
+ * Se usa como contrato independiente de la jerarquía de herencia,
+ * permitiendo que cualquier criatura adopte la capacidad de volar
+ * sin modificar la clase base.
+ */
+public interface Volador {
 
+    /**
+     * Hace que la criatura despegue y vuele.
+     * Cada implementación define cómo realiza el vuelo.
+     */
+    void volar();
+
+    /**
+     * Hace que la criatura aterrice.
+     * Cada implementación define cómo aterriza.
+     */
+    void aterrizar();
 }
